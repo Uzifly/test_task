c7e8497a971b8baeb04a1b71d61438e49ee862f0
Uzifly <111077672+Uzifly@users.noreply.github.com>
2024-03-10 22:42:58 +0300

    Update payments_mart.sql

--- test_dbt/models/views/payments_mart.sql
+++ test_dbt/models/views/payments_mart.sql
@@ -3,16 +3,3 @@ SELECT *,
     ROW_NUMBER() OVER (PARTITION BY client_id ORDER BY payment_date) AS case_number,
     SUM(value) OVER (PARTITION BY DATE_TRUNC('month', payment_date)) AS revenue
 FROM {{ source('test_db', 'payments') }}
-/*
-SELECT client_name, payment_date, value, 
-       MAX(value) OVER (partition by client_name) AS max_value
-FROM payments
---WHERE DATE_TRUNC('month', payment_date) = '2023-01-01'
-
-SELECT *, ROW_NUMBER() OVER (PARTITION BY client_id ORDER BY payment_date) AS case_number 
-FROM payments
-
-SELECT DATE_TRUNC('month', payment_date) AS period, SUM(value) AS revenue
-FROM payments
---GROUP BY period
-*/
